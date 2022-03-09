a01c3651992fb2d255b19b744178950e5585bcf2
PomiNdor <shishkov-011@bk.ru>
2022-03-09 22:00:44 +0300

    Add an interface
    Adding a standard interface for stack implementation

---
+++ Stack/Stack/IStack.cs
@@ -0,0 +1,12 @@
+﻿namespace CRiUPP_lab_01
+{
+  public interface IStack<T>
+  {
+    int Count { get; }
+    void Push(T item);
+    T Peek();
+    T Pop();
+    void Clear();
+    bool Contains(T item);
+  }
+}
